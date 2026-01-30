c49f879de700ba29b1c26d3104c76ff62a801cd6
kevzn <paledust@hotmail.com>
2026-01-31 00:07:18 +0800

    update head control

--- Assets/Scripts/FaceControl.cs
+++ Assets/Scripts/FaceControl.cs
@@ -19,7 +19,15 @@ public class FaceControl : MonoBehaviour
     {
         Vector3 localPos = faceTrans.parent.InverseTransformPoint(cursorTrans.position);
         Vector3 diff = localPos - initLocalPos;
-        Quaternion target = Quaternion.Euler(0, 0, Mathf.Atan(diff.y/Mathf.Abs(diff.x)) * Mathf.Rad2Deg * diff.magnitude/rotateFade);
+        Quaternion target = Quaternion.Euler(0, 0, 0);
+        if(diff.x>0)
+        {
+            target = Quaternion.Euler(0, 0, Mathf.Atan(diff.y/Mathf.Abs(diff.x)) * Mathf.Rad2Deg * diff.magnitude/rotateFade);
+        }
+        else
+        {
+            target = Quaternion.Euler(0, 0, - Mathf.Atan(diff.y/Mathf.Abs(diff.x)) * Mathf.Rad2Deg * diff.magnitude/rotateFade);
+        }
         localPos.z = initLocalPos.z;
         Vector3 targetPos = initLocalPos + (localPos - initLocalPos) * followFactor;
         faceTrans.localPosition = Vector3.Lerp(faceTrans.localPosition, targetPos, Time.deltaTime * followLerp);
