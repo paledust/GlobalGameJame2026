d05a65aa14b447cee3e851369f49b409ff0bfab5
kevzn <paledust@hotmail.com>
2026-01-30 23:59:23 +0800

    update head eye control

---
+++ Assets/Scripts/BodyControl.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BodyControl : MonoBehaviour
+{
+    [SerializeField] private Transform faceTrans;
+    [SerializeField] private Transform bodyTrans;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/FaceControl.cs
+++ Assets/Scripts/FaceControl.cs
@@ -3,15 +3,26 @@ using UnityEngine;
 public class FaceControl : MonoBehaviour
 {
     [SerializeField] private Transform cursorTrans;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Transform faceTrans;
+    [Header("Follow")]
+    [SerializeField] private float followFactor = 0.1f;
+    [SerializeField] private float followLerp = 5f;
+    [Header("Rotation")]
+    [SerializeField] private float rotateFade = 0.5f;
+    [SerializeField] private float rotateRange = 15f;
+    private Vector3 initLocalPos;
+    void OnEnable()
     {
-        
+        initLocalPos = faceTrans.localPosition;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 localPos = faceTrans.parent.InverseTransformPoint(cursorTrans.position);
+        Vector3 diff = localPos - initLocalPos;
+        Quaternion target = Quaternion.Euler(0, 0, Mathf.Atan(diff.y/Mathf.Abs(diff.x)) * Mathf.Rad2Deg * diff.magnitude/rotateFade);
+        localPos.z = initLocalPos.z;
+        Vector3 targetPos = initLocalPos + (localPos - initLocalPos) * followFactor;
+        faceTrans.localPosition = Vector3.Lerp(faceTrans.localPosition, targetPos, Time.deltaTime * followLerp);
+        faceTrans.localRotation = Quaternion.Slerp(faceTrans.localRotation, target, Time.deltaTime * followLerp);
     }
 }
